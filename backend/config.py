91813125f36994434901c4731c098859292479b3
Prasad Panchal <81217516+Prasadpanchal@users.noreply.github.com>
2025-06-12 22:50:58 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 # File: backend/config.py
 
 DB_USER = 'postgres'
-DB_PASS = 'Prasad%40123'
-DB_NAME = 'autopay_db'
+DB_PASS = 'Database Password'
+DB_NAME = 'autopay_db' # Database name
 DB_HOST = 'localhost'
 
 SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
@@ -12,12 +12,12 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 
-# हे Flask-Mail च्या ऑथेंटिकेशनसाठी अत्यंत महत्त्वाचे आहेत
-MAIL_USERNAME = 'prasadpanchalps@gmail.com' # <--- हे तुमचे Gmail ऍड्रेस असावे
-MAIL_PASSWORD = 'bwsl vohd qlog kjap'       # <--- हा 'prasadpanchalps@gmail.com' साठीचा ऍप पासवर्ड असावा
+# These are extremely important for Flask-Mail authentication.
+MAIL_USERNAME = 'Sender Mail id' # Sender Mail id
+MAIL_PASSWORD = 'Your_app_password'         # Sender App Password
 
-# तुम्ही तुमच्या सोयीनुसार हे अतिरिक्त व्हेरिएबल्स ठेवू शकता
-HARDCODED_RECEIVER_EMAIL = 'prasadpanchalps@gmail.com'
-SENDER_EMAIL = 'prasadpanchalps@gmail.com'
+# You can keep these additional variables as per your convenience.
+HARDCODED_RECEIVER_EMAIL = 'Receiver Mail id'  # Receiver Mail id
+SENDER_EMAIL = 'Sender Mail' # Sender Mail 
 
-MAIL_DEFAULT_SENDER = 'prasadpanchalps@gmail.com'
+MAIL_DEFAULT_SENDER = 'Sender Mail' # Sender Mail
